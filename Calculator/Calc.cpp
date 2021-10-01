a821141d9264592ca4ed3f58351370db45e0a406
Semen <Semen@LAPTOP-0KHEF962>
2021-10-01 23:43:09 +0300

    Добавьте файлы проекта.

---
+++ Calculator/Calc.cpp
@@ -0,0 +1,18 @@
+#include "Calc.h"
+
+double Calc::Calculate(double x, char oper, double y)
+{
+    switch (oper)
+    {
+    case '+':
+        return x + y;
+    case '-':
+        return x - y;
+    case '*':
+        return x * y;
+    case '/':
+        return x / y;
+    default:
+        return 0.0;
+    }
+}
---
+++ Calculator/Calc.h
@@ -0,0 +1,7 @@
+#pragma once
+class Calc
+{
+public:
+    double Calculate(double x, char oper, double y);
+};
+
